966e7c5548f6ecd5c1a06c971969bb2ad08e50e1
alejandra.diaz <alejandra.diaz@ixcomercio.com>
2022-11-20 19:29:11 -0300

    modifica Jenkinsfile

--- maven.groovy
+++ maven.groovy
@@ -5,9 +5,7 @@ def build_test(){
 
 def run_app(){
   echo "run..."
-  sh "./mvnw spring-boot:run &"
-  sh 'sleep 5'
-  sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+  sh "./mvnw spring-boot:run  > /tmp/mscovid.log 2>&1 &"
 }
 
 return this
